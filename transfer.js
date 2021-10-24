bac874be644b24ed2adeb124988a2f05c5877ccc
Istallia <55042065+istallia@users.noreply.github.com>
2021-10-24 10:40:48 +0900

    [Add] URLからIDを取れるように

--- transfer.js
+++ transfer.js
@@ -13,3 +13,22 @@ const NICONICO_URLS = {
 	gm : 'game.nicovideo.jp/atsumaru/games/%id%',
 	nq : 'q.nicovideo.jp/watch/%id%'
 };
+
+
+/* --- 転送を行う --- */
+browser.webRequest.onBeforeRequest.addListener(details => {
+	let work_id = details.url.match(/\b(sm|so|lv|nc|td|co|gm|nq)\d{1,12}/);
+	if (work_id) work_id = work_id[0];
+}, {
+	urls : [
+		"https://www.nicovideo.jp/search/*",
+		"https://www.nicovideo.jp/tag/*",
+		"https://seiga.nicovideo.jp/search/*",
+		"https://live.nicovideo.jp/search",
+		"https://commons.nicovideo.jp/search/keyword/*",
+		"https://3d.nicovideo.jp/search",
+		"https://com.nicovideo.jp/search/*",
+		"https://game.nicovideo.jp/atsumaru/search/word/*",
+		"https://q.nicovideo.jp/search"
+	]
+}, ['blocking']);
